d88a0ffc1c22e8b626ad1d4202bf3fac58da2cea
vexfairyprincess <alejandromoramndz@gmail.com>
2023-04-12 15:56:38 -0600

    update

--- HwFor/5.6.cpp
+++ HwFor/5.6.cpp
@@ -4,26 +4,30 @@ mayor que 5).*/
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int cantidad, nota, aprobados = 0;
-    
+
     cout << "Ingrese la cantidad de calificaciones: ";
     cin >> cantidad;
-    
-    for (int i = 0; i < cantidad; i++) {
-        cout << "Ingrese la nota: " << i+1 << ": ";
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout << "Ingrese la nota: " << i + 1 << ": ";
         cin >> nota;
-        
-        if (nota >= 5) {
+
+        if (nota >= 5)
+        {
             aprobados++;
         }
-        if (nota > 10 || nota < 0){
-            cout << "Haz introducido una nota invalida" <<endl;
+        if (nota > 10 || nota < 0)
+        {
+            cout << "Haz introducido una nota invalida" << endl;
             return 0;
         }
     }
-    
+
     cout << "El número total de aprobados es: " << aprobados << endl;
-    
+
     return 0;
 }
--- HwFor/5.7.cpp
+++ HwFor/5.7.cpp
@@ -3,30 +3,32 @@ todas aquellas que son NOTABLES (>= 7 y < 9).*/
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int cantidad, nota;
-    
+
     cout << "Ingrese la cantidad de notas: ";
     cin >> cantidad;
-    
-    for (int i = 1; i <= cantidad; i++) {
+
+    for (int i = 1; i <= cantidad; i++)
+    {
         cout << "Ingrese la nota " << i << ": ";
         cin >> nota;
-        
-        if (nota >= 7 && nota < 9) {
+
+        if (nota >= 7 && nota < 9)
+        {
             cout << "La nota " << nota << " es notable" << endl;
         }
-        if(nota > 10 || nota < 0 )
+        if (nota > 10 || nota < 0)
         {
             cout << "Haz ingresado una nota invalida" << endl;
             return 0;
         }
-       
     }
-    if(nota < 7){
-            cout << "No hay ninguna nota notable" << endl;
+    if (nota < 7)
+    {
+        cout << "No hay ninguna nota notable" << endl;
+    }
 
-        }
-    
     return 0;
 }
