e0d5d3f5ff4efae34a14900bff4863a897df788f
vexfairyprincess <alejandromoramndz@gmail.com>
2023-04-13 23:39:27 -0600

    update

--- HwFor/5.4.cpp
+++ HwFor/5.4.cpp
@@ -3,28 +3,26 @@ terminada con un número no positivo después del último número válido.*/
 #include <iostream>
 
 using namespace std;
-int main() {
+int main()
+{
     float numero = 0, suma = 0, cont = 0;
-    for(int i = 0 ; i<= cont; i++)
+    for (int i = 0; i <= cont; i++)
     {
-    double media;
-    suma += numero;
-    media = suma / cont;
-    cout << "Introduce una lista de números positivos, para finalizar de introducir numeros ingresa un # negativo" << endl;
-    cin >> numero;
-    
-    if(numero < 0)
-    {
-        cout << "La media de esta lista es de " << media << endl;
-        break;
-    }else if(numero >0)
-    {
-        cont++;
+        double media;
+        suma += numero;
+        media = suma / cont;
+        cout << "Introduce una lista de números positivos, para finalizar de introducir numeros ingresa un # negativo" << endl;
+        cin >> numero;
 
-
-    }
-
-
+        if (numero < 0)
+        {
+            cout << "La media de esta lista es de " << media << endl;
+            break;
+        }
+        else if (numero > 0)
+        {
+            cont++;
+        }
     }
     return 0;
 }
